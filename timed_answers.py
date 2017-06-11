45b0483de1f8f027985eeeec8fc78949e2887f6a
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-06-10 22:06:57 -0700

    Actually save time to answer config and respect it.

--- timed_answers.py
+++ timed_answers.py
@@ -1,6 +1,7 @@
 from anki.hooks import addHook, wrap
 from aqt import mw
 from aqt.qt import *
+from aqt.deckconf import DeckConf
 from aqt.forms import dconf
 from aqt.utils import showInfo
 
@@ -17,19 +18,39 @@ def customFormSetupUi(self, *args, **kwargs):
     self.gridLayout_3.addWidget(spinBox, row, 1)
     self.gridLayout_3.addWidget(QLabel('seconds'), row, 2)
 
+    self.timeToAnswer = spinBox
+
     return ret
 
-def time_limit_hit():
+def customDeckConfSaveConf(self, *args, **kwargs):
+    self.conf['rev']['timeToAnswer'] = self.form.timeToAnswer.value()
+
+def customDeckConfLoadConf(self, *args, **kwargs):
+    self.form.timeToAnswer.setValue(self.conf['rev'].get('timeToAnswer', 0))
+
+def timeLimitHit():
     mw.reviewer._showAnswer()
 
-def start_timer():
+def startTimer():
     card = mw.reviewer.card
-    deck_conf = mw.col.decks.confForDid(card.odid or card.did)
-    answer_time = deck_conf.get('answer_time', 0)
+    deckConf = mw.col.decks.confForDid(card.odid or card.did)
+    answerTime = deckConf['rev'].get('timeToAnswer', 0)
 
-    if answer_time:
-        mw.progress.timer(answer_time * 1000, time_limit_hit, False)
+    if answerTime:
+        mw.progress.timer(answerTime * 1000, timeLimitHit, False)
 
-dconf.Ui_Dialog.setupUi = wrap(dconf.Ui_Dialog.setupUi,
-                               customFormSetupUi, 'around')
-addHook('showQuestion', start_timer)
+DeckConf.saveConf = wrap(
+    DeckConf.saveConf,
+    customDeckConfSaveConf,
+    'before')
+
+DeckConf.loadConf = wrap(
+    DeckConf.loadConf,
+    customDeckConfLoadConf,
+    'after')
+
+dconf.Ui_Dialog.setupUi = wrap(
+    dconf.Ui_Dialog.setupUi,
+    customFormSetupUi,
+    'around')
+addHook('showQuestion', startTimer)
