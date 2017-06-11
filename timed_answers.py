803cba8721442ce3293d753000bd7eb9d542d4c8
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-06-10 21:51:01 -0700

    Added time to answer settings field that does nothing.

--- timed_answers.py
+++ timed_answers.py
@@ -1,13 +1,35 @@
-import threading
-
-from anki.hooks import addHook
-from aqt.utils import showInfo
+from anki.hooks import addHook, wrap
 from aqt import mw
+from aqt.qt import *
+from aqt.forms import dconf
+from aqt.utils import showInfo
+
+def customFormSetupUi(self, *args, **kwargs):
+    _old = kwargs['_old']
+    del kwargs['_old']
+    ret = _old(self, *args, **kwargs)
+
+    row = self.gridLayout_3.rowCount()
+    spinBox = QSpinBox()
+    spinBox.setMinimum(0)
+
+    self.gridLayout_3.addWidget(QLabel('Time to Answer'), row, 0)
+    self.gridLayout_3.addWidget(spinBox, row, 1)
+    self.gridLayout_3.addWidget(QLabel('seconds'), row, 2)
+
+    return ret
 
 def time_limit_hit():
     mw.reviewer._showAnswer()
 
 def start_timer():
-    mw.progress.timer(5000, time_limit_hit, False)
+    card = mw.reviewer.card
+    deck_conf = mw.col.decks.confForDid(card.odid or card.did)
+    answer_time = deck_conf.get('answer_time', 0)
 
+    if answer_time:
+        mw.progress.timer(answer_time * 1000, time_limit_hit, False)
+
+dconf.Ui_Dialog.setupUi = wrap(dconf.Ui_Dialog.setupUi,
+                               customFormSetupUi, 'around')
 addHook('showQuestion', start_timer)
