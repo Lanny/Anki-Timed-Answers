0347a1bc37e37ae61bf0edc98a80c59de0458bdf
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-06-17 17:45:46 -0700

    Add countdown timer.

--- timed_answers.py
+++ timed_answers.py
@@ -1,25 +1,89 @@
+import random
+
 from anki.hooks import addHook, wrap
 from aqt import mw
+from aqt.reviewer import Reviewer
 from aqt.qt import *
 from aqt.deckconf import DeckConf
 from aqt.forms import dconf
 from aqt.utils import showInfo
+
+LIVE_TIMER = None
+
+timerJs = """
+    ;(function(seconds, killHook) {
+        var startTime = Date.now();
+        var timerElement = $('<span class="answer-timer">')
+            .prependTo('table .stat:first')
+            .css('margin-left', '5px');
+
+        window.killHooks = window.killHooks || {};
+        window.killHooks[killHook] = function() {
+            timerElement.remove();
+            delete window.killHooks[killHook];
+        };
+
+        function formatTime(seconds) {
+            var minutes = ~~(seconds / 60),
+                seconds = ('0' + ~~(seconds %% 60)).substr(-2);
+
+            return minutes + ':' + seconds;
+        }
+
+
+        ;(function tick() {
+            var timeRemaining = seconds - (Date.now() - startTime) / 1000;
+
+            if (timeRemaining <= 0) {
+                if (killHook in window.killHooks) {
+                    window.killHooks[killHook]();
+                }
+
+                return;
+            }
+
+            timerElement.text(formatTime(timeRemaining));
+
+            setTimeout(tick, 1000);
+        })();
+    })(%s, '%s');
+"""
 
 class AnswerTimer(object):
     def __init__(self, card):
         self._card = card
         deckConf = mw.col.decks.confForDid(card.odid or card.did)
         self.answerTime = deckConf['rev'].get('timeToAnswer', 0)
+        self.killHook = str(random.randint(0, 2**32))
+        self.timerHasBeenInjected = False
 
     def isNeeded(self):
         return bool(self.answerTime)
 
     def start(self):
         mw.progress.timer(self.answerTime * 1000, self._timeLimitUp, False)
+        self.injectTimer()
+
+    def onQuestionAnswered(self):
+        mw.bottomWeb.eval('window.killHooks["%s"]()' % self.killHook);
+
+    def injectTimer(self):
+        if (self.timerHasBeenInjected):
+            return
+
+        self.timerHasBeenInjected = True
+        mw.bottomWeb.eval(timerJs % (self.answerTime, self.killHook));
+
+    def _isRolling(self):
+        return mw.reviewer.state == 'question' and mw.reviewer.card == self._card
+
+    def _advanceTimer(self):
+        if self._isRolling():
+            mw.progress.timer(1000, self._advanceTimer, False)
 
     def _timeLimitUp(self):
         # Make sure we haven't answered or moved onto a different question.
-        if mw.reviewer.state == 'question' and mw.reviewer.card == self._card:
+        if self._isRolling():
             mw.reviewer._showAnswer()
 
 def customFormSetupUi(self, *args, **kwargs):
@@ -46,11 +110,21 @@ def customDeckConfLoadConf(self, *args, **kwargs):
     self.form.timeToAnswer.setValue(self.conf['rev'].get('timeToAnswer', 0))
 
 def startTimer():
-    card = mw.reviewer.card
-    timer = AnswerTimer(card)
+    global LIVE_TIMER
 
-    if timer.isNeeded():
-        timer.start()
+    card = mw.reviewer.card
+
+    if (LIVE_TIMER and LIVE_TIMER._card == card):
+        return
+
+    LIVE_TIMER = AnswerTimer(card)
+
+    if LIVE_TIMER.isNeeded():
+        LIVE_TIMER.start()
+
+def stopTimer():
+    global LIVE_TIMER
+    LIVE_TIMER.onQuestionAnswered()
 
 DeckConf.saveConf = wrap(
     DeckConf.saveConf,
@@ -62,8 +136,14 @@ DeckConf.loadConf = wrap(
     customDeckConfLoadConf,
     'after')
 
+Reviewer._showAnswerButton = wrap(
+    Reviewer._showAnswerButton,
+    lambda *args, **kwargs: startTimer(),
+    'after')
+
 dconf.Ui_Dialog.setupUi = wrap(
     dconf.Ui_Dialog.setupUi,
     customFormSetupUi,
     'around')
-addHook('showQuestion', startTimer)
+
+addHook('showAnswer', stopTimer)
